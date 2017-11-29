c382e654d6a3e163f3cec625262e28f7770237ac
hnxyhcwdl1003@163.com <hnxyhcwdl1003@163.com>
2017-11-29 08:41:15 +0800

    修改gitignore

---
+++ src/main/java/org/flywaydb/spring/boot/ext/FlywayDataSourceWrapper.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2010-2020, vindell (https://github.com/vindell).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.flywaydb.spring.boot.ext;
+
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+public class FlywayDataSourceWrapper implements DataSource {
+
+	private DataSource delegate;
+	
+	public FlywayDataSourceWrapper(DataSource org) {
+		this.delegate = org;
+	}
+	
+	@Override
+	public PrintWriter getLogWriter() throws SQLException {
+		return delegate.getLogWriter();
+	}
+
+	@Override
+	public void setLogWriter(PrintWriter out) throws SQLException {
+		delegate.setLogWriter(out);
+	}
+
+	@Override
+	public void setLoginTimeout(int seconds) throws SQLException {
+		delegate.setLoginTimeout(seconds);
+	}
+
+	@Override
+	public int getLoginTimeout() throws SQLException {
+		return delegate.getLoginTimeout();
+	}
+
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		return delegate.getParentLogger();
+	}
+
+	@Override
+	public <T> T unwrap(Class<T> iface) throws SQLException {
+		return delegate.unwrap(iface);
+	}
+
+	@Override
+	public boolean isWrapperFor(Class<?> iface) throws SQLException {
+		return delegate.isWrapperFor(iface);
+	}
+
+	@Override
+	public Connection getConnection() throws SQLException {
+		return delegate.getConnection();
+	}
+
+	@Override
+	public Connection getConnection(String username, String password) throws SQLException {
+		return delegate.getConnection(username, password);
+	}
+
+}
